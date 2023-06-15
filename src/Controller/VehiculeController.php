705195909f7ac1b607dddd64ca99569d7f0d7caf
ericchow8@gmail.com <ericchow8@gmail.com>
2023-06-15 17:05:47 +0200

    update product view page and reservation success

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -2,21 +2,80 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Membre;
+use App\Entity\Commande;
+use App\Entity\Vehicule;
+use App\Form\CommandeType;
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 // !App Controller
 class VehiculeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(VehiculeRepository $repo, Request $request, EntityManagerInterface $manager): Response
     {
+        $vehicules = $repo->findAll($repo);
+
         return $this->render('vehicule/index.html.twig', [
-            'controller_name' => 'VehiculeController',
+            
+            'vehicules' => $vehicules
         ]);
+
+        
     }
+        #[Route('/view/{id}', name:'view')]
+        public function view(Vehicule $vehicule, Request $request, EntityManagerInterface $manager): Response
+        {
+            if($vehicule == null)
+        {
+            return $this->redirectToRoute('home');
+        }
+
+        if($this->getUser())
+        {
+            $membre = $this->getUser();
+        }
+        
+
+        $commande = new Commande;
+
+        $formCommande = $this->createForm(CommandeType::class, $commande);
+        $formCommande->handleRequest($request);
+
+        if($formCommande->isSubmitted() && $formCommande->isValid())
+        {
+            
+            $dateDepart = $commande->getDateHeureDepart();
+            $dateFin    = $commande->getDateHeureFin();
+            $nombreJour = date_diff($dateDepart,$dateFin);
+
+            $prixtotal  = $vehicule->getPrixJournalier()*$nombreJour->d;
+
+            $commande   ->setDateEnregistrement(new \DateTime())
+                        ->setMembre($membre)
+                        ->setVehicule($vehicule)
+                        ->setPrixTotal($prixtotal);
+
+            $manager->persist($commande);
+            $manager->flush();
+        
+            $this->addFlash('success', 'Votre commande a été bien enregistré');
+
+            return $this->redirectToRoute('home');      
+        }
+            return $this->render('vehicule/view.html.twig',[
+                'formCommande' => $formCommande->createView(),
+                'vehicule' => $vehicule
+            ]);
+        }
+    
 
     
 }
